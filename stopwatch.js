34a4fd8d34f3af66df568b55d594ddda2596d105
Erik Sabin <eriksabin97@gmail.com>
2022-06-20 11:55:20 -0400

    I add two buttons. One to add a time and one to count down. Currently the cound down doesnt work and when I try to count up the timer text goes crazy. Fix pending soon

--- stopwatch.js
+++ stopwatch.js
@@ -9,15 +9,39 @@ let hour = 0;
 let time = h + ':' + m + ":" + s;
 console.log(time);
 let inter;
+let interDown;
+
+
 
 function stopInterval() {
     clearInterval(inter);
 }
 
+function timerDown(){
+
+   
+   second = second - 1 ;
+   document.querySelector('.seconds').textContent = second;
+
+    if(second == 0){
+        second = 60
+        minute = minute + 1;
+        document.querySelector('.minutes').textContent = minute + ":";
+    
+    }
+    else if(minute == 0){
+        minute = 60
+        hour = hour + 1;
+        document.querySelector('.hours').textContent = hour + ':';
+    
+    }
+    
+}
+
 function mytimer(){
-  
-    second = second + 1;
     document.querySelector('.seconds').textContent = second;
+    second = second + 1;
+    
     if(second == 60){
         second = 0
         minute = minute + 1;
@@ -49,4 +73,22 @@ document.querySelector('.minutes').textContent = minute + ':';
 document.querySelector('.hours').textContent = hour +':';
     stopInterval();
 
+})
+
+document.querySelector('.down').addEventListener('click', function(){
+    second = document.querySelector('.countDownSeconds').value;
+minute = document.querySelector('.countDownMinutes').value;
+hour = document.querySelector('.countDownHours').value;
+
+    document.querySelector('.hours').textContent = hour + ':';
+    document.querySelector('.minutes').textContent = minute + ':';
+    document.querySelector('.seconds').textContent = second;
+    
+
+  
+})
+
+document.querySelector('.countDown').addEventListener('click', function(){
+    interDown = setInveral(timerDown, 1000);
+    timerDown();
 })
